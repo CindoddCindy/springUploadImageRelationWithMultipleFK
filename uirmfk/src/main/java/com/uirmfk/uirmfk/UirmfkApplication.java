3001ded401b8c0f1bd4e62c9319d7631e14c58c5
CindoddCindy <cindodcindy@gmail.com>
2021-02-09 14:45:54 +0700

    baru bikin package

---
+++ uirmfk/src/main/java/com/uirmfk/uirmfk/UirmfkApplication.java
@@ -0,0 +1,13 @@
+package com.uirmfk.uirmfk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UirmfkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UirmfkApplication.class, args);
+	}
+
+}
---
+++ uirmfk/src/test/java/com/uirmfk/uirmfk/UirmfkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uirmfk.uirmfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UirmfkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
